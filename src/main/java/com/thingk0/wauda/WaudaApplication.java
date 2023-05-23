e0977510e194035c04dd4b377ac9642e456c433a
Thingk0 <audtjd9080@naver.com>
2023-05-23 16:55:04 +0900

    Initial commit for WAUDA project:)

---
+++ src/main/java/com/thingk0/wauda/WaudaApplication.java
@@ -0,0 +1,13 @@
+package com.thingk0.wauda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaudaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaudaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thingk0/wauda/WaudaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thingk0.wauda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaudaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
