ae8472c85511c577a98f9863a1758ca853ed6146
Thingk0 <audtjd9080@naver.com>
2023-06-02 18:50:47 +0900

    :sparkles: feat: 로그인 실패 시 사용자 알림 및 리다이렉트 처리 구현
    - CustomAuthenticationFailureHandler 클래스 추가
    - 로그인 실패 시, 이메일 또는 비밀번호 오류 알림 메시지 및 예외 로직 설정

---
+++ src/main/java/com/thingk0/wauda/handler/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,29 @@
+package com.thingk0.wauda.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception)
+            throws IOException, ServletException {
+
+        log.error("로그인 실패: 아이디나 비밀번호가 잘못되었습니다.");
+
+        // 로그인 실패에 대한 로직 구현.
+        request.getSession().setAttribute("errorMessage", "로그인 실패: 아이디나 비밀번호가 잘못되었습니다.");
+        // 로그인 실패 시 리다이렉트 할 URL 지정
+        response.sendRedirect("/members/login");
+    }
+}
+
