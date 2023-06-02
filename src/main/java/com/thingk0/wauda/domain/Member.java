520debcf81ec114dfec729fd6b56276163a1270d
Thingk0 <audtjd9080@naver.com>
2023-06-02 18:33:00 +0900

    :sparkles: feat: Member 엔티티 생성 시 기본값으로 Role.USER 설정

--- src/main/java/com/thingk0/wauda/domain/Member.java
+++ src/main/java/com/thingk0/wauda/domain/Member.java
@@ -2,7 +2,7 @@ package com.thingk0.wauda.domain;
 
 import com.thingk0.wauda.domain.base.BaseEntity;
 import com.thingk0.wauda.domain.constant.Role;
-import com.thingk0.wauda.dto.MemberRegisterDto;
+import com.thingk0.wauda.dto.RegisterDto;
 import lombok.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -32,14 +32,16 @@ public class Member extends BaseEntity {
 
 
     /**
-     * MemberRegisterDto -> Member 엔티티로 변환.
-     * PasswordEncoder 를 통해 비밀번호 암호화해서 변환.
+     * Member 생성 메서드
+     * @param dto 회원가입 dto
+     * @param encoder 패스워드 인코더
      */
-    public static Member create(MemberRegisterDto dto, PasswordEncoder encoder) {
+    public static Member create(RegisterDto dto, PasswordEncoder encoder) {
         return Member.builder()
                 .email(dto.getEmail())
                 .password(encoder.encode(dto.getPassword()))
                 .nickname(dto.getNickname())
+                .role(Role.USER)
                 .build();
     }
 
