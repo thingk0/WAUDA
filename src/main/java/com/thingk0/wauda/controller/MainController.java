08c39b75c77125abbc98ccad25ec79a455970ee6
Thingk0 <audtjd9080@naver.com>
2023-06-02 18:35:30 +0900

    :sparkles: feat: 검색창 표시 여부를 위한 값 추가.
    - 앞으로 애플리케이션에서 검색창 표시 여부를 선택적으로 결정할 수 있도록 showHeader 값을 활용할 예정.

--- src/main/java/com/thingk0/wauda/controller/MainController.java
+++ src/main/java/com/thingk0/wauda/controller/MainController.java
@@ -1,14 +1,15 @@
 package com.thingk0.wauda.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
-
     @GetMapping(value = "/")
-    public String mainPage() {
+    public String mainPage(Model model) {
+        model.addAttribute("showHeader", true);
         return "home";
     }
 
