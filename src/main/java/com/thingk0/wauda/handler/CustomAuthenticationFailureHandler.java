c5f46a4d8cebc342905aa6808a54d28ed6ca94df
Thingk0 <audtjd9080@naver.com>
2023-06-05 14:37:43 +0900

    :truck: chore: 커스텀 리포지터리 추가와 로그인 실패 시 리다이렉트 URL 변경.
    - 이전 커밋에서 Querydsl을 활용한 파티 목록 조회용 동적 쿼리를 추가했으나 Repository 인터페이스에 누락되어, 함께 다시 커밋함.
    - 로그인 실패 시 리다이렉트되는 URL을 적절히 수정함.

--- src/main/java/com/thingk0/wauda/handler/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/thingk0/wauda/handler/CustomAuthenticationFailureHandler.java
@@ -23,7 +23,7 @@ public class CustomAuthenticationFailureHandler implements AuthenticationFailure
         // 로그인 실패에 대한 로직 구현.
         request.getSession().setAttribute("errorMessage", "로그인 실패: 아이디나 비밀번호가 잘못되었습니다.");
         // 로그인 실패 시 리다이렉트 할 URL 지정
-        response.sendRedirect("/members/login");
+        response.sendRedirect("/members/login/error");
     }
 }
 
--- src/main/java/com/thingk0/wauda/repository/PartyRepository.java
+++ src/main/java/com/thingk0/wauda/repository/PartyRepository.java
@@ -1,7 +1,8 @@
 package com.thingk0.wauda.repository;
 
 import com.thingk0.wauda.domain.Party;
+import com.thingk0.wauda.repository.party.CustomPartyRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PartyRepository extends JpaRepository<Party, Long> {
+public interface PartyRepository extends JpaRepository<Party, Long>, CustomPartyRepository {
 }
