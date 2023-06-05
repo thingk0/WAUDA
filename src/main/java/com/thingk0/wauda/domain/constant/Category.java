b47ec28060261612a3fd7c09882ae54a2df2dcf7
Thingk0 <audtjd9080@naver.com>
2023-06-05 13:53:10 +0900

    :sparkles: feat: 파티 카테고리별 한글 value 값 추가
    - 상수형 영문 필드 값은 코드에서 사용되고, 클라이언트에는 한글 표기를 위해 value 추가함.

--- src/main/java/com/thingk0/wauda/domain/constant/Category.java
+++ src/main/java/com/thingk0/wauda/domain/constant/Category.java
@@ -1,10 +1,20 @@
 package com.thingk0.wauda.domain.constant;
 
+import lombok.Getter;
+
+@Getter
 public enum Category {
-    TAXI,
-    MEAL,
-    CAFE,
-    MOVIE,
-    EXERCISE,
-    HOBBY
+
+    TAXI("택시팟"),
+    MEAL("밥팟"),
+    CAFE("카페팟"),
+    MOVIE("영화팟"),
+    EXERCISE("운동팟"),
+    HOBBY("취미팟");
+
+    String value;
+
+    Category(String value) {
+        this.value = value;
+    }
 }
