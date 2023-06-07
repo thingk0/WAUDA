d31ebea3f7e426e4082790f3225fa3a676827d05
Thingk0 <audtjd9080@naver.com>
2023-06-07 11:17:57 +0900

    :sparkles: feat: 프로필 버튼을 다양한 URL과 매칭하기 위한 수정.
    - footer.js: 로그인, 회원가입, 프로필 URL과 매칭을 위해 자바스크립트에서 배열을 사용하여 처리하도록 변경.

--- src/main/resources/static/js/footer.js
+++ src/main/resources/static/js/footer.js
@@ -3,7 +3,11 @@ let buttons = document.querySelectorAll('.footer-btn');
 function changeButtonStyle() {
     let currentUrl = window.location.pathname;
     buttons.forEach((btn, idx) => {
-        if (btn.dataset.url === currentUrl) {
+
+        // 프로필 버튼 매칭 URL 추가
+        const profileUrls = ['/members/login', '/members/signup', '/members/profile'];
+
+        if (btn.dataset.url === currentUrl || (btn.dataset.url === '/members/login' && profileUrls.includes(currentUrl))) {
             btn.classList.add('button-blue');
             btn.classList.add('button-border');
         } else {
