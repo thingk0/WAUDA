64ffaeb2ae0726f7d8f4deeb1dbdb5eb017617b9
Thingk0 <audtjd9080@naver.com>
2023-06-16 19:18:35 +0900

    :sparkles: feat: 댓글 작성 UI 구현.
    - footer.html: 컨트롤러로부터 commentEnabled 값을 전달 받을 경우, 즉 파티 상세 페이지에서만 댓글 작성 가능한 UI/UX가 나타나도록 구현.
    - footer.js: URL 패턴 매칭 기능을 포함하고 댓글 UI를 위한 로직을 추가.
    - basic.css: 댓글 입력 필드를 포함한 CSS 스타일을 구현.

--- src/main/resources/static/js/footer.js
+++ src/main/resources/static/js/footer.js
@@ -2,12 +2,16 @@ let buttons = document.querySelectorAll('.footer-btn');
 
 function changeButtonStyle() {
     let currentUrl = window.location.pathname;
+
+    // '/party/{id}'와 매칭하는 정규표현식
+    const partyPattern = new RegExp('^/party/\\d+$');
+    // '/members/**'와 매칭하는 정규표현식
+    const memberPattern = new RegExp('^/members/.*$');
+
     buttons.forEach((btn, idx) => {
-
-        // 프로필 버튼 매칭 URL 추가
-        const profileUrls = ['/members/login', '/members/signup', '/members/profile'];
-
-        if (btn.dataset.url === currentUrl || (btn.dataset.url === '/members/login' && profileUrls.includes(currentUrl))) {
+        if (btn.dataset.url === currentUrl ||
+            (memberPattern.test(currentUrl) && idx == 4) ||
+            (partyPattern.test(currentUrl) && idx === 0)) {
             btn.classList.add('button-blue');
             btn.classList.add('button-border');
         } else {
@@ -30,3 +34,18 @@ buttons.forEach((button, index) => {
         window.location.href = url;
     });
 });
+
+
+document.querySelector('.custom-input-form input').addEventListener('focus', function() {
+    const imgElem = document.querySelector('.custom-input-form button img');
+    imgElem.style.opacity = "1";
+    imgElem.style.filter = "invert(20%) sepia(100%) saturate(10000%) hue-rotate(210deg)";
+});
+
+
+document.querySelector('.custom-input-form input').addEventListener('blur', function() {
+    const imgElem = document.querySelector('.custom-input-form button img');
+    imgElem.style.opacity = "0.3";
+    imgElem.style.filter = "";  // 필요한 기본 필터 스타일로 변경하세요
+});
+
