6b014dfa4489c727bdbb3bc3a9e4922b8c2b2ea0
Thingk0 <audtjd9080@naver.com>
2023-06-25 14:17:53 +0900

    :truck: chore: 메서드 호출 추적을 위해 로깅 코드 추가

--- src/main/java/com/thingk0/wauda/service/MemberService.java
+++ src/main/java/com/thingk0/wauda/service/MemberService.java
@@ -7,12 +7,14 @@ import com.thingk0.wauda.exception.EmailAlreadyExistsException;
 import com.thingk0.wauda.exception.ProfileNotFoundException;
 import com.thingk0.wauda.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -23,6 +25,7 @@ public class MemberService {
 
     @Transactional(readOnly = true)
     public Member findMemberByEmail(String email) {
+        log.info("MemberService.findMemberByEmail");
         return memberRepository.findByEmail(email).orElseThrow(
                 () -> new EntityNotFoundException("해당 이메일의 회원을 찾을 수 없습니다.")
         );
@@ -30,12 +33,14 @@ public class MemberService {
 
     @Transactional
     public Profile getProfile(String email) throws ProfileNotFoundException {
+        log.info("MemberService.getProfile");
         return memberRepository.getProfileByEmail(email).orElseThrow(
                 () -> new ProfileNotFoundException("해당 아이디의 회원 프로필을 찾을 수 없습니다.")
         );
     }
 
     public Long register(RegisterDto registerDto) throws EmailAlreadyExistsException {
+        log.info("MemberService.register");
         if (memberRepository.existsByEmail(registerDto.getEmail()))
             throw new EmailAlreadyExistsException("이미 존재하는 이메일입니다.");
 
--- src/main/java/com/thingk0/wauda/service/PartyService.java
+++ src/main/java/com/thingk0/wauda/service/PartyService.java
@@ -6,11 +6,15 @@ import com.thingk0.wauda.dto.party.PartyListDto;
 import com.thingk0.wauda.dto.party.PartyResponseDto;
 import com.thingk0.wauda.repository.PartyRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
+
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -20,17 +24,28 @@ public class PartyService {
     private final MemberService memberService;
 
     public Long create(PartyForm partyForm, String email) {
+        log.info("PartyService.create");
         return partyRepository.save(Party.create(partyForm, memberService.findMemberByEmail(email))).getId();
     }
 
     @Transactional(readOnly = true)
     public Page<PartyListDto> getPartyList(String search, Pageable pageable) {
+        log.info("PartyService.getPartyList");
         return partyRepository.partyList(search, pageable);
     }
 
     @Transactional(readOnly = true)
     public PartyResponseDto getPartyDetail(Long id) {
+        log.info("PartyService.getPartyDetail");
         return partyRepository.partyDetail(id);
+    }
+
+    @Transactional(readOnly = true)
+    public Party findPartyById(Long id) {
+        log.info("PartyService.findPartyById");
+        return partyRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Party Not Found")
+        );
     }
 
 }
