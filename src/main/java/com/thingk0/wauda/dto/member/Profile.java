b92d192a42451592d28071ff647dddd56e00f9c9
Thingk0 <audtjd9080@naver.com>
2023-06-25 14:16:46 +0900

    :truck: chore: Lombok 사용하여 프로필 코드 간소화

--- src/main/java/com/thingk0/wauda/dto/member/Profile.java
+++ src/main/java/com/thingk0/wauda/dto/member/Profile.java
@@ -1,20 +1,18 @@
 package com.thingk0.wauda.dto.member;
 
 import com.thingk0.wauda.domain.constant.Role;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Profile {
 
     private Long id;
     private String email;
     private String nickname;
     private Role role;
-
-    public Profile(Long id, String email, String nickname, Role role) {
-        this.id = id;
-        this.email = email;
-        this.nickname = nickname;
-        this.role = role;
-    }
 }
